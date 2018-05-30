1ffe6168be044c89188e9914780660e1a753bb5f
Ville Heilala <ville@heila.la>
2018-05-30 14:34:16 +0300

    Add fi

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -1,7 +1,8 @@
 #
 # Copy the slides and the exercise files referenced from the slides from the
 # master branch to the gh-pages branch to make them accessible at
-# http://jdleesmiller.github.io/latex-course/...
+# http://villeheilala.github.io/latex-course/...
+# Original repository at http://jdleesmiller.github.io/latex-course/...
 #
 
 if ! git diff-index --quiet HEAD --; then
@@ -11,6 +12,10 @@ fi
 
 # build / bundle files that we need to put on the website
 cd en
+make latex-course.tgz
+cd ..
+
+cd "fi"
 make latex-course.tgz
 cd ..
 
@@ -26,14 +31,22 @@ tar xzf latex-course.tgz
 rm latex-course.tgz
 cd ..
 
-echo '<html><head><title>latex-course</title></head><body><a href="https://github.com/jdleesmiller/latex-course">Please see repository.</a></body></html>' > index.html
+mkdir "fi"
+cd "fi"
+mv ../../"fi"/latex-course.tgz .
+tar xzf latex-course.tgz
+rm latex-course.tgz
+cd ..
+
+echo '<html><head><title>latex-course</title></head><body><a href="https://github.com/villeheilala/latex-course">Please see repository.</a></body></html>' > index.html
 
 cd ..
 
 # destroy current content of the gh-pages branch
-git checkout gh-pages
+git checkout -f  gh-pages
 git rm -rf .
 rm -rf en
+rm -rf "fi"
 mv deploy/* .
 rmdir deploy
 git add .
